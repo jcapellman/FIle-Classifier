8d8f4ad0829fffd298530abf5dbb23b4980ddc90
Jarred Capellman <jcapellman@hotmail.com>
2019-04-27 13:27:53 -0500

    Added: Initial commit of the Classifier class

---
+++ src/FileClassifier.lib/Classifier.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using FileClassifier.lib.Common;
+
+namespace FileClassifier.lib
+{
+    public class Classifier
+    {
+        public static bool IsMalicious(Options option)
+        {
+            if (option.Verbose)
+            {
+                Console.WriteLine($"Classifying {option.FileName}");
+            }
+
+            return false;
+        }
+    }
+}
