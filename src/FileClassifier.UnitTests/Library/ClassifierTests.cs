1a582e7a31faf4fdab032ccff244cff9553549a5
Jarred Capellman <jcapellman@hotmail.com>
2019-04-27 18:17:52 -0500

    Added: Additional Unit Tests to cover the Classifier

--- src/FileClassifier.UnitTests/Library/ClassifierTests.cs
+++ src/FileClassifier.UnitTests/Library/ClassifierTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 
 using FileClassifier.lib;
+using FileClassifier.lib.Common;
 
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -14,6 +16,23 @@ namespace FileClassifier.UnitTests.Library
         public void NullOptions()
         {
             Classifier.IsMalicious(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullFileNameOptions()
+        {
+            Classifier.IsMalicious(new Options());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void FileDoesntExistOptions()
+        {
+            Classifier.IsMalicious(new Options
+            {
+                FileName = DateTime.Now.Ticks.ToString()
+            });
         }
     }
 }
