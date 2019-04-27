cb25c3e2bafb10ed035182e3d345f38ab855607c
Jarred Capellman <jcapellman@hotmail.com>
2019-04-27 17:46:25 -0500

    Added: Initial work on the ClassifierTests

---
+++ src/FileClassifier.UnitTests/Library/ClassifierTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using FileClassifier.lib;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileClassifier.UnitTests.Library
+{
+    [TestClass]
+    public class ClassifierTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void NullOptions()
+        {
+            Classifier.IsMalicious(null);
+        }
+    }
+}
