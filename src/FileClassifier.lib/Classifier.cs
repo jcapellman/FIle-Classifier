292520bf46b8f6f318694f1e3649a490a85da51e
Jarred Capellman <jcapellman@hotmail.com>
2019-04-27 18:20:19 -0500

    Added: SanityCheckOptions method to ensure before processing options are valid

--- src/FileClassifier.lib/Classifier.cs
+++ src/FileClassifier.lib/Classifier.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 using FileClassifier.lib.Common;
 
@@ -6,16 +7,31 @@ namespace FileClassifier.lib
 {
     public class Classifier
     {
-        public static bool IsMalicious(Options option)
+        internal static void SanityCheckOptions(Options option)
         {
             if (option is null)
             {
                 throw new ArgumentNullException(nameof(option));
             }
 
+            if (string.IsNullOrEmpty(option.FileName))
+            {
+                throw new ArgumentNullException(nameof(option.FileName));
+            }
+
+            if (!File.Exists(option.FileName))
+            {
+                throw new FileNotFoundException($"{option.FileName} was not found...");
+            }
+        }
+
+        public static bool IsMalicious(Options option)
+        {
+            SanityCheckOptions(option);
+
             if (option.Verbose)
             {
-                Console.WriteLine($"Classifying {option.FileName}");
+                Console.WriteLine($"Classifying {option.FileName}...");
             }
 
             return false;
