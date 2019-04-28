892992bdde2bb7b92b3c3f9f3e8e16352a499cc4
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 09:14:22 -0500

    Added: ClassifierResponseItem Class

---
+++ src/FileClassifier.lib/Common/ClassifierResponseItem.cs
@@ -0,0 +1,22 @@
+﻿using FileClassifier.lib.Enums;
+
+namespace FileClassifier.lib.Common
+{
+    public class ClassifierResponseItem
+    {
+        public string SHA1Hash { get; set; }
+
+        public double Confidence { get; set; }
+
+        public FileGroupType FileGroup { get; set; }
+
+        public ClassifierResponseItem()
+        {
+            FileGroup = FileGroupType.UNKNOWN;
+
+            Confidence = 0.0;
+        }
+
+        public override string ToString() => $"SHA1: {SHA1Hash} | File Group: {FileGroup} | Confidence: {Confidence}";
+    }
+}
