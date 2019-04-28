9d033e69527a5ae1b35dbaab753a9b50e03b1554
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 09:29:23 -0500

    Added: Hashing Extension Class

---
+++ src/FileClassifier.lib/Helpers/Hashing.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace FileClassifier.lib.Helpers
+{
+    public static class Hashing
+    {
+        public static string ToSHA1(this byte[] data)
+        {
+            if (data == null || data.Length == 0)
+            {
+                return Common.Constants.RESPONSE_ERROR;
+            }
+
+            using (var sha = new SHA1Managed())
+            {
+                return BitConverter.ToString(sha.ComputeHash(data)).Replace("-", "");
+            }
+        }
+    }
+}
