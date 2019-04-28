9f75a94de689e3b9b91ad6a5d8bbf887e7ee40d3
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 17:29:18 -0500

    Added: Initial commit of the ClusterDataPrediction class

---
+++ src/FileClassifier.lib/ML/Clustering/Objects/ClusterDataPrediction.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace FileClassifier.lib.ML.Clustering.Objects
+{
+    public class ClusterDataPrediction
+    {
+        [ColumnName("PredictedLabel")]
+        public uint PredictedClusterId;
+
+        [ColumnName("Score")]
+        public float[] Distances;
+    }
+}
