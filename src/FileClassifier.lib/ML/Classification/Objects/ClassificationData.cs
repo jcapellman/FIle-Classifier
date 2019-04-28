386fa75e4fa75724deac11ea47e343e96b6430ce
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 18:33:09 -0500

    Added: Initial commit of the ClassificationData class #2

---
+++ src/FileClassifier.lib/ML/Classification/Objects/ClassificationData.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.ML.Data;
+
+namespace FileClassifier.lib.ML.Classification.Objects
+{
+    public class ClassificationData
+    {
+        [LoadColumn(0)]
+        public string NGramText;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Malicious;
+    }
+}
