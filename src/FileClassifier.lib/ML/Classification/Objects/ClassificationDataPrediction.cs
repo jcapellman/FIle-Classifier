cf5f97a7629477c051165820c0e4934065d148af
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 18:33:30 -0500

    Added: Initial commit of the ClassificationDataPrediction class #2

---
+++ src/FileClassifier.lib/ML/Classification/Objects/ClassificationDataPrediction.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.ML.Data;
+
+namespace FileClassifier.lib.ML.Classification.Objects
+{
+    public class ClassificationDataPrediction : ClassificationData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
