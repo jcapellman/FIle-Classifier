0b089d7a7828235de526d805ba7e45e8916eaddc
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 09:13:29 -0500

    Added: FileGroupType Enum

---
+++ src/FileClassifier.lib/Enums/FileGroupType.cs
@@ -0,0 +1,12 @@
+﻿namespace FileClassifier.lib.Enums
+{
+    public enum FileGroupType
+    {
+        EXECUTABLE,
+        IMAGE,
+        VIDEO,
+        DOCUMENT,
+        SCRIPT,
+        UNKNOWN
+    }
+}
