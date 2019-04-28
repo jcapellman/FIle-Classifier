7a3a215f011759d673265f2845d45ed98dc56d40
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 09:28:06 -0500

    Added: HashingTests Unit Test class

---
+++ src/FileClassifier.UnitTests/Library/Helpers/HashingTests.cs
@@ -0,0 +1,40 @@
+﻿using FileClassifier.lib.Helpers;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FileClassifier.UnitTests.Library.Helpers
+{
+    [TestClass]
+    public class HashingTests
+    {
+        [TestMethod]
+        public void SHA1_NullBytes()
+        {
+            byte[] data = null;
+
+            var result = data.ToSHA1();
+
+            Assert.AreEqual(lib.Common.Constants.RESPONSE_ERROR, result);
+        }
+
+        [TestMethod]
+        public void SHA1_EmptyBytes()
+        {
+            byte[] data = new byte[0];
+
+            var result = data.ToSHA1();
+
+            Assert.AreEqual(lib.Common.Constants.RESPONSE_ERROR, result);
+        }
+
+        [TestMethod]
+        public void SHA1_TestBytes()
+        {
+            byte[] data = new byte[3] { 0x3c, 0x4d, 0x5e };
+
+            var result = data.ToSHA1();
+
+            Assert.AreEqual("2C2A964127CCF7D9A8B00362BAC95FD9EBE95238", result);
+        }
+    }
+}
