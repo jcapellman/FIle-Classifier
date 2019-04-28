6e674304171ccc7655ae1a205f252356901d9e61
Jarred Capellman <jcapellman@hotmail.com>
2019-04-28 17:28:57 -0500

    Added: Initial commit of the ClusterData class

---
+++ src/FileClassifier.lib/ML/Clustering/Objects/ClusterData.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.ML.Data;
+
+namespace FileClassifier.lib.ML.Clustering.Objects
+{
+    public class ClusterData
+    {
+        [LoadColumn(0)]
+        public string Grams;
+    }
+}
