1f03f808873bc938974264d7c81dcb57ad8630e4
Jarred Capellman <jcapellman@hotmail.com>
2019-04-29 22:03:15 -0500

    Added: BasePrediction class

---
+++ src/FileClassifier.lib/ML/Base/BasePrediction.cs
@@ -0,0 +1,15 @@
+﻿using FileClassifier.lib.Common;
+
+using Microsoft.ML;
+
+namespace FileClassifier.lib.ML.Base
+{
+    public abstract class BasePrediction
+    {
+        protected abstract string MODEL_NAME { get; }
+
+        protected static readonly MLContext MlContext = new MLContext(Common.Constants.ML_SEED);
+
+        public abstract ClassifierResponseItem Predict(ClassifierResponseItem response);
+    }
+}
