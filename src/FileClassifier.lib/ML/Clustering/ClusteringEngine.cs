498eb209ad513038ddf1a80e73f51bff8b0b7a64
Jarred Capellman <jcapellman@hotmail.com>
2019-05-02 21:20:22 -0500

    Added: Initial work on the TrainModel for the ClusteringEngine class

--- src/FileClassifier.lib/ML/Clustering/ClusteringEngine.cs
+++ src/FileClassifier.lib/ML/Clustering/ClusteringEngine.cs
@@ -1,7 +1,11 @@
-﻿using FileClassifier.lib.Common;
+﻿using System.IO;
+
+using FileClassifier.lib.Common;
 using FileClassifier.lib.Enums;
 using FileClassifier.lib.ML.Base;
 using FileClassifier.lib.ML.Clustering.Objects;
+
+using Microsoft.ML;
 
 namespace FileClassifier.lib.ML.Clustering
 {
@@ -19,6 +23,26 @@ namespace FileClassifier.lib.ML.Clustering
         public override ClusterData FeatureExtraction(ClassifierResponseItem response)
         {
             return new ClusterData();
+        }
+
+        public override bool TrainModel(string dataFile)
+        {
+            var dataView = MlContext.Data.LoadFromTextFile<ClusterData>(dataFile, hasHeader: false, separatorChar: ',');
+
+            var featuresColumnName = "Features";
+
+            var pipeline = MlContext.Transforms
+                .Concatenate(featuresColumnName, "Grams")
+                .Append(MlContext.Clustering.Trainers.KMeans(featuresColumnName, numberOfClusters: 3));
+
+            var model = pipeline.Fit(dataView);
+
+            using (var fileStream = new FileStream(MODEL_NAME, FileMode.Create, FileAccess.Write, FileShare.Write))
+            {
+                MlContext.Model.Save(model, dataView.Schema, fileStream);
+            }
+
+            return true;
         }
     }
 }
