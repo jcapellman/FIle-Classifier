ce5aaff571d48290d45a56cbf6da5e43f4de11d5
Jarred Capellman <jcapellman@hotmail.com>
2019-05-03 19:38:06 -0500

    Added: Initial TrainerCommandLineOptions class

---
+++ src/FileClassifier.lib/Common/TrainerCommandLineOptions.cs
@@ -0,0 +1,11 @@
+﻿namespace FileClassifier.lib.Common
+{
+    public class TrainerCommandLineOptions
+    {
+        public string FolderOfData { get; set; }
+
+        public bool Verbose { get; set; }
+
+        public override string ToString() => $"Folder Containing Data: {FolderOfData} | Verbose: {Verbose}";
+    }
+}
