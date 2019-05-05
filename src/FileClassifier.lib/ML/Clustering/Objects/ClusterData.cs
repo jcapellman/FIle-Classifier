4277dcdffa5de8d767d35442cc1d96c76c484caa
Jarred Capellman <jcapellman@hotmail.com>
2019-05-04 19:18:25 -0500

    Added: Label property in ClusterData class

--- src/FileClassifier.lib/ML/Clustering/Objects/ClusterData.cs
+++ src/FileClassifier.lib/ML/Clustering/Objects/ClusterData.cs
@@ -1,15 +1,13 @@
 ﻿using FileClassifier.lib.ML.Base.Objects;
 
-using Microsoft.ML.Data;
-
 namespace FileClassifier.lib.ML.Clustering.Objects
 {
     public class ClusterData : BaseData
     {
-        [LoadColumn(0)]
+        public float Label;
+
         public float Size;
 
-        [LoadColumn(1)]
         public float Grams;
     }
 }
