86b4e16ab14532bde89a901a05572b452c564135
Jarred Capellman <jcapellman@hotmail.com>
2019-05-07 20:26:23 -0500

    Added: Initial work on the Spark Worker

---
+++ src/FileClassifier.SparkWorker/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Spark.Sql;
+
+namespace FileClassifier.SparkWorker
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var spark = SparkSession.Builder().GetOrCreate();
+        }
+    }
+}
