74f692b6bdf6f4a3c71228aea5ff03af93f47817
Jarred Capellman <jcapellman@hotmail.com>
2019-05-10 21:55:40 -0500

    Added: Implemented the ClassificationEngine class

--- src/FileClassifier.lib/ML/Classification/ClassificationEngine.cs
+++ src/FileClassifier.lib/ML/Classification/ClassificationEngine.cs
@@ -1,7 +1,12 @@
-﻿using FileClassifier.lib.Common;
+﻿using System;
+
+using FileClassifier.lib.Base;
+using FileClassifier.lib.Common;
 using FileClassifier.lib.ML.Base;
 using FileClassifier.lib.ML.Classification.Objects;
 using FileClassifier.lib.Options;
+
+using Microsoft.ML;
 
 namespace FileClassifier.lib.ML.Classification
 {
@@ -19,12 +24,44 @@ namespace FileClassifier.lib.ML.Classification
 
         public override (ClassificationData Data, string Output) FeatureExtraction(ClassifierResponseItem response)
         {
-            return (new ClassificationData(), string.Empty);
+            var classificationData = new ClassificationData
+            {
+                NGramText = GetStrings(response.Data, 0, response.Data.Length),
+                Malicious = false
+            };
+
+            return (classificationData, $"{classificationData.NGramText},{classificationData.Malicious}");
         }
 
         public override bool TrainModel(TrainerCommandLineOptions options)
         {
-            throw new System.NotImplementedException();
+            var fileName = FeatureExtractFolder(options);
+
+            var startDate = DateTime.Now;
+
+            var dataView = MlContext.Data.LoadFromTextFile<ClassificationData>(fileName, hasHeader: false);
+
+            var splitDataView = MlContext.Data.TrainTestSplit(dataView, testFraction: 0.2);
+
+            var featuresColumnName = "Features";
+
+            var estimator = MlContext.Transforms.Text.FeaturizeText(outputColumnName: featuresColumnName,
+                inputColumnName: nameof(ClassificationData.NGramText))
+                .Append(MlContext.BinaryClassification.Trainers.SdcaLogisticRegression(labelColumnName: "Label", featureColumnName: featuresColumnName));
+
+            var model = estimator.Fit(splitDataView.TrainSet);
+
+            Logger<TrainerCommandLineOptions>.Debug($"Model trained in {DateTime.Now.Subtract(startDate).TotalSeconds} seconds", options);
+
+            var predictions = model.Transform(splitDataView.TestSet);
+
+            var metrics = MlContext.BinaryClassification.Evaluate(predictions, "Label");
+
+            Logger<TrainerCommandLineOptions>.Debug($"Accuracy: {metrics.Accuracy} | F1: {metrics.F1Score} | Auc: {metrics.AreaUnderRocCurve}", options);
+
+            SaveModel(model, splitDataView.TrainSet.Schema, options);
+
+            return true;
         }
     }
 }
