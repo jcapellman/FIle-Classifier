3fc53999b2e2d3289eb4d16acef692d2494cbd45
Jarred Capellman <jcapellman@hotmail.com>
2019-05-29 20:36:39 -0500

    Added: Initial work on the LiteDBDatabase class

---
+++ src/FileClassifier.JobManager.REST/Databases/LiteDBDatabase.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using FileClassifier.JobManager.REST.Databases.Base;
+using FileClassifier.JobManager.REST.Databases.Tables;
+
+using LiteDB;
+
+namespace FileClassifier.JobManager.REST.Databases
+{
+    public class LiteDBDatabase : IDatabase
+    {
+        private const string DB_FILENAME = "data.db";
+
+        public bool DeleteJob(Guid id)
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                return db.GetCollection<Jobs>().Delete(a => a.ID == id) > 0;
+            }
+        }
+
+        public bool AddJob(Jobs item)
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                return db.GetCollection<Jobs>().Insert(item) != null;
+            }
+        }
+
+        public bool UpdateJob(Jobs item)
+        {
+            using (var db = new LiteDatabase(DB_FILENAME))
+            {
+                return db.GetCollection<Jobs>().Update(item);
+            }
+        }
+    }
+}
