b1771f62bedad1fcd88d67509e22dc6dca325f98
Jarred Capellman <jcapellman@hotmail.com>
2019-05-29 20:36:07 -0500

    Added: Initial commit of the IDatabase Interface

---
+++ src/FileClassifier.JobManager.REST/Databases/Base/IDatabase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+using FileClassifier.JobManager.REST.Databases.Tables;
+
+namespace FileClassifier.JobManager.REST.Databases.Base
+{
+    public interface IDatabase
+    {
+        bool DeleteJob(Guid id);
+
+        bool AddJob(Jobs item);
+
+        bool UpdateJob(Jobs item);
+    }
+}
