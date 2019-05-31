5d5bbbd8676ff73c609b040b30b2055ddf4bc5c2
Jarred Capellman <jcapellman@hotmail.com>
2019-05-30 22:00:09 -0500

    Added: Initial commit of the Uploader

---
+++ src/FileClassifier.JobManager.Uploader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileClassifier.JobManager.Uploader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             // TODO: handle command line submissions
+        }
+    }
+}
