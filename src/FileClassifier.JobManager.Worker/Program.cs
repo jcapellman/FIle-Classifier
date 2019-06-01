494eae04290810b7227a8cbea0aa113f26e7c1ac
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 15:06:37 -0500

    Added: Initial work on the Program class of the Worker app

---
+++ src/FileClassifier.JobManager.Worker/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace FileClassifier.JobManager.Worker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serverUrl = "http://localhost:5000/api/";
+
+            if (args.Any())
+            {
+                serverUrl = args[0];
+            }
+
+            new Worker(serverUrl).RunAsync();
+        }
+    }
+}
