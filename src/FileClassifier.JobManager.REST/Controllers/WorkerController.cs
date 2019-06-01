e98fdf658407f3c07ca81bcfc6e192f8acb14f11
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 15:36:59 -0500

    Added: Initial commit of the WorkerController class

---
+++ src/FileClassifier.JobManager.REST/Controllers/WorkerController.cs
@@ -0,0 +1,18 @@
+﻿using FileClassifier.JobManager.lib.Databases.Base;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileClassifier.JobManager.REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WorkerController : ControllerBase
+    {
+        private readonly IDatabase _database;
+
+        public WorkerController(IDatabase database)
+        {
+            _database = database;
+        }
+    }
+}
