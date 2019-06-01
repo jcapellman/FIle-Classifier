e576b88d705cdb2deefe4dbafb1ce5c256f82c0c
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 13:36:20 -0500

    Added: Initial work on the HostController class

---
+++ src/FileClassifier.JobManager.REST/Controllers/HostController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using FileClassifier.JobManager.lib.Databases.Base;
+using FileClassifier.JobManager.lib.Databases.Tables;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileClassifier.JobManager.REST.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HostController : ControllerBase
+    {
+        private IDatabase _database;
+
+        [HttpPost]
+        public void AddUpdateHost(Hosts host)
+        {
+            _database.AddUpdateHost(host);       
+        }
+
+        [HttpDelete]
+        public void DeleteHost(Guid id)
+        {
+            _database.DeleteHost(id);
+        }
+    }
+}
