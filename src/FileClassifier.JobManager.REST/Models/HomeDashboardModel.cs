19435b66b372ff2f34a61075cf2701cf0e154672
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 13:43:03 -0500

    Added: Initial commit of the HomeDashboardModel class

---
+++ src/FileClassifier.JobManager.REST/Models/HomeDashboardModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using FileClassifier.JobManager.lib.Databases.Tables;
+
+namespace FileClassifier.JobManager.REST.Models
+{
+    public class HomeDashboardModel
+    {
+        public List<Hosts> Hosts { get; set; }
+        
+        public List<Jobs> Jobs { get; set; }
+    }
+}
