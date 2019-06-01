eb382b156912f635a2c713a446199c657ec56d5d
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 13:36:46 -0500

    Added: Initial work on the Hosts class

---
+++ src/FileClassifier.JobManager.lib/Databases/Tables/Hosts.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FileClassifier.JobManager.lib.Databases.Tables
+{
+    public class Hosts
+    {
+        public Guid ID { get; set; }
+
+        public string Name { get; set; }
+
+        public int NumCores { get; set; }
+
+        public int NumGBOfRam { get; set; }
+
+        public DateTime LastConnected { get; set; }
+        
+        public string WorkerVersion { get; set; }
+    }
+}
