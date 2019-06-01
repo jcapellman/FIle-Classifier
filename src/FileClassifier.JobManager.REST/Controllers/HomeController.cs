6982dd37bb2e16911c87a0e2bada7179a8c1cfa4
Jarred Capellman <jcapellman@hotmail.com>
2019-05-31 21:48:33 -0500

    Added: Initial work on the Views and HomeController class

---
+++ src/FileClassifier.JobManager.REST/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using FileClassifier.JobManager.lib.Databases.Base;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace FileClassifier.JobManager.REST.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IDatabase _database;
+
+        public HomeController(IDatabase database)
+        {
+            _database = database;
+        }
+
+        public IActionResult Index() => View("Index", _database.GetJobs());
+    }
+}
