d95ea498ee9e73bc6ca61f5771794cfb60728bb6
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 16:15:24 -0500

    Added: Program class for the Uploader

--- src/FileClassifier.JobManager.Uploader/Program.cs
+++ src/FileClassifier.JobManager.Uploader/Program.cs
@@ -1,12 +1,41 @@
 ﻿using System;
+using System.Threading.Tasks;
+
+using FileClassifier.JobManager.lib.Databases.Tables;
+using FileClassifier.JobManager.lib.Handlers;
+using FileClassifier.lib.Enums;
 
 namespace FileClassifier.JobManager.Uploader
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-             // TODO: handle command line submissions
+            // Eventually force the use of a json file
+            if (args.Length < 4)
+            {
+                Console.WriteLine($"Usage is:{Environment.NewLine}FileClassifier.JobManager.Uploader <name> <model type> <path to data> <server url>");
+
+                return;
+            }
+
+            if (!Enum.TryParse<ModelType>(args[1], true, out _))
+            {
+                Console.WriteLine($"Invalid Model Type option ({args[1]})");
+
+                return;
+            }
+
+            var jobHandler = new JobHandler(args[3]);
+
+            var result = await jobHandler.AddNewJobAsync(new Jobs
+            {
+                ModelType = args[1],
+                TrainingDataPath = args[2],
+                Name = args[0]
+            });
+
+            Console.WriteLine(result ? "Job successfully uploaded" : "Failed to upload job");
         }
     }
 }
