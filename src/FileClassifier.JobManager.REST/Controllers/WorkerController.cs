8ffe7b75d7769ab7c73b89c858a443736fa1dbc0
Jarred Capellman <jcapellman@hotmail.com>
2019-06-01 21:17:09 -0500

    Added: GetWork method in the WorkerController

--- src/FileClassifier.JobManager.REST/Controllers/WorkerController.cs
+++ src/FileClassifier.JobManager.REST/Controllers/WorkerController.cs
@@ -1,4 +1,9 @@
-﻿using FileClassifier.JobManager.lib.Databases.Base;
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using FileClassifier.JobManager.lib.Common;
+using FileClassifier.JobManager.lib.Databases.Base;
+using FileClassifier.JobManager.lib.Databases.Tables;
 
 using Microsoft.AspNetCore.Mvc;
 
@@ -13,6 +18,30 @@ namespace FileClassifier.JobManager.REST.Controllers
         public WorkerController(IDatabase database)
         {
             _database = database;
+        }
+
+        public List<Jobs> GetWork(string host)
+        {
+            var assignedJobs = _database.GetJobs().Where(a => a.AssignedHost == host).ToList();
+
+            if (assignedJobs.Any())
+            {
+                return assignedJobs;
+            }
+
+            var unassignedJob = _database.GetJobs().FirstOrDefault(a => a.Name == Constants.UNASSIGNED_JOB);
+
+            // No jobs available
+            if (unassignedJob == null)
+            {
+                return new List<Jobs>();
+            }
+
+            // Assign the first unassigned job to the host
+            unassignedJob.Name = host;
+            _database.UpdateJob(unassignedJob);
+
+            return new List<Jobs> { unassignedJob };
         }
     }
 }
