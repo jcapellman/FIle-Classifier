71963dd22e929e60d7785b4c60c9d69f3c6c6692
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 08:02:37 -0500

    Added: CheckinWorker Class for refactoring

---
+++ src/FileClassifier.JobManager.Worker/BackgroundWorkers/CheckinWorker.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel;
+
+using FileClassifier.JobManager.lib.Databases.Tables;
+using FileClassifier.JobManager.lib.Handlers;
+using FileClassifier.JobManager.Worker.Common;
+
+namespace FileClassifier.JobManager.Worker.BackgroundWorkers
+{
+    public class CheckinWorker
+    {
+        private BackgroundWorker _bwCheckin;
+
+        private Hosts _host;
+
+        private readonly string _serverURL;
+
+        public CheckinWorker()
+        {
+            _bwCheckin = new BackgroundWorker();
+            _bwCheckin.DoWork += BwCheckin_DoWork;
+            _bwCheckin.RunWorkerCompleted += BwCheckin_RunWorkerCompleted;            
+        }
+
+        public void Run(Hosts host, string serverURL)
+        {
+            _host = host;
+
+            _bwCheckin.RunWorkerAsync();
+        }
+
+        private void BwCheckin_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            System.Threading.Thread.Sleep(Constants.LOOP_INTERVAL_MS);
+
+            _bwCheckin.RunWorkerAsync();
+        }
+
+        private async void BwCheckin_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var hostHandler = new HostsHandler(_serverURL);
+
+            // Call to checkin with the server
+            var checkinResult = await hostHandler.AddUpdateHostAsync(_host);
+
+            if (!checkinResult)
+            {
+                Console.WriteLine($"Failed to check in with {_serverURL}");
+
+                System.Threading.Thread.Sleep(Constants.LOOP_ERROR_INTERVAL_MS);
+            }
+        }
+    }
+}
