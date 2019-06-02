08771555a5b7f6ebc5b1a1254126894617749f21
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 10:19:28 -0500

    Change: Refactored the Worker class to simply call the Job/Worker related code

--- src/FileClassifier.JobManager.Worker/Worker.cs
+++ src/FileClassifier.JobManager.Worker/Worker.cs
@@ -1,16 +1,9 @@
 ﻿using System;
-using System.IO;
 using System.Reflection;
 
 using FileClassifier.JobManager.lib.Databases.Tables;
-using FileClassifier.JobManager.lib.Handlers;
 using FileClassifier.JobManager.Worker.BackgroundWorkers;
 using FileClassifier.JobManager.Worker.Common;
-
-using FileClassifier.lib.Enums;
-using FileClassifier.lib.ML.Classification;
-using FileClassifier.lib.ML.Clustering;
-using FileClassifier.lib.Options;
 
 namespace FileClassifier.JobManager.Worker
 {
@@ -21,6 +14,7 @@ namespace FileClassifier.JobManager.Worker
         private readonly Hosts _host;
 
         private readonly CheckinWorker _cWorker = new CheckinWorker();
+        private readonly JobWorker _jWorker = new JobWorker();
 
         public Worker(string serverURL)
         {
@@ -38,61 +32,19 @@ namespace FileClassifier.JobManager.Worker
         public async void RunAsync()
         {
             _cWorker.Run(_host, _serverURL);
-
-            var workerHandler = new WorkerHandler(_serverURL);
-
+            
             while (true)
             {
-                var work = await workerHandler.GetWorkAsync(_host.Name);
+                var workerResult = await _jWorker.Run(_host, _serverURL);
 
-                if (work == null)
+                if (!workerResult)
+                {
+                    System.Threading.Thread.Sleep(Constants.LOOP_ERROR_INTERVAL_MS);
+                }
+                else
                 {
                     System.Threading.Thread.Sleep(Constants.LOOP_INTERVAL_MS);
-
-                    continue;
                 }
-
-                work.Started = true;
-                work.StartTime = DateTime.Now;
-
-                var result = await workerHandler.UpdateWorkAsync(work);
-
-                if (!result)
-                {
-                    System.Threading.Thread.Sleep(Constants.LOOP_INTERVAL_MS);
-
-                    continue;
-                }
-
-                var options = new TrainerCommandLineOptions
-                {
-                    FolderOfData = work.TrainingDataPath,
-                    LogLevel = LogLevels.DEBUG
-                };
-
-                var outputModelPath = string.Empty;
-
-                switch (Enum.Parse(typeof(ModelType), work.ModelType))
-                {
-                    case ModelType.CLASSIFICATION:
-                        outputModelPath = new ClassificationEngine().TrainModel(options);
-                        break;
-                    case ModelType.CLUSTERING:
-                        outputModelPath = new ClusteringEngine().TrainModel(options);
-                        break;
-                }
-
-                if (File.Exists(outputModelPath))
-                {
-                    work.Model = File.ReadAllBytes(outputModelPath);
-                }
-
-                work.Completed = true;
-                work.CompletedTime = DateTime.Now;
-
-                _ = await workerHandler.UpdateWorkAsync(work);
-
-                System.Threading.Thread.Sleep(Constants.LOOP_INTERVAL_MS);
             }
         }
     }
