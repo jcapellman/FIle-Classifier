206c1a3c0992edae4648368faae17ee3851b242a
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 08:03:05 -0500

    Added: Constants class for the Worker app

---
+++ src/FileClassifier.JobManager.Worker/Common/Constants.cs
@@ -0,0 +1,11 @@
+﻿namespace FileClassifier.JobManager.Worker.Common
+{
+    public class Constants
+    {
+        public const int LOOP_INTERVAL_MS = 60000;
+
+        public const int LOOP_ERROR_INTERVAL_MS = LOOP_INTERVAL_MS * 5;
+
+        public const string DEFAULT_SERVER_URL = "http://localhost:5000/api/";
+    }
+}
