247dde19e9d0d7ad95c7fe7be3e8b8c96f92b109
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 16:17:37 -0500

    Added: JobHandler class for the Uploader app

---
+++ src/FileClassifier.JobManager.lib/Handlers/JobHandler.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using FileClassifier.JobManager.lib.Databases.Tables;
+
+using Newtonsoft.Json;
+
+namespace FileClassifier.JobManager.lib.Handlers
+{
+    public class JobHandler
+    {
+        private readonly string _rootUrl;
+
+        public JobHandler(string rootURL)
+        {
+            _rootUrl = rootURL;
+        }
+
+        public async Task<bool> AddNewJobAsync(Jobs job)
+        {
+            try
+            {
+                using (var httpClient = new HttpClient())
+                {
+                    httpClient.BaseAddress = new Uri(_rootUrl);
+
+                    var json = JsonConvert.SerializeObject(job);
+
+                    var stringContent = new StringContent(json, Encoding.UTF8, "application/json");
+
+                    var response = httpClient.PostAsync("Job", stringContent).Result;
+
+                    var responseBody = await response.Content.ReadAsStringAsync();
+
+                    return response.IsSuccessStatusCode;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return false;
+            }
+        }
+    }
+}
