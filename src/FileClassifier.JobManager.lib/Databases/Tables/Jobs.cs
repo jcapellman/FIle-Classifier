a21ecd4461928f3472877f9e946e208072dacfc7
Jarred Capellman <jcapellman@hotmail.com>
2019-06-02 10:00:44 -0500

    Added: Additional columns to the Jobs Table class

--- src/FileClassifier.JobManager.lib/Databases/Tables/Jobs.cs
+++ src/FileClassifier.JobManager.lib/Databases/Tables/Jobs.cs
@@ -10,10 +10,20 @@ namespace FileClassifier.JobManager.lib.Databases.Tables
 
         public string Name { get; set; }
 
+        public string TrainingDataPath { get; set; }
+
+        public string ModelType { get; set; }
+
         public DateTime SubmissionTime { get; set; }
 
         public DateTime CompletedTime { get; set; }
 
+        public DateTime StartTime { get; set; }
+
+        public bool Started { get; set; }
+
         public bool Completed { get; set; }
+
+        public byte[] Model { get; set; }
     }
 }
