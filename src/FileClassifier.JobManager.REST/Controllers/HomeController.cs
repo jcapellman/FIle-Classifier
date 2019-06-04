a22a3801d4aa40fc292ba4e1db936d1f289d7471
Jarred Capellman <jcapellman@hotmail.com>
2019-06-03 19:53:32 -0500

    Added: AddJob Form #31

--- src/FileClassifier.JobManager.REST/Controllers/HomeController.cs
+++ src/FileClassifier.JobManager.REST/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 
 using FileClassifier.JobManager.lib.Databases.Base;
+using FileClassifier.JobManager.lib.Databases.Tables;
 using FileClassifier.JobManager.REST.Models;
 
 using Microsoft.AspNetCore.Mvc;
@@ -20,6 +22,51 @@ namespace FileClassifier.JobManager.REST.Controllers
             Jobs = _database.GetJobs(),
             Hosts = _database.GetHosts()
         });
+
+        [HttpGet]
+        public IActionResult AddJob([FromQuery]string name, [FromQuery]string trainingDataPath, [FromQuery]string modelType)
+        {
+            var job = new Jobs
+            {
+                ID = Guid.NewGuid(),
+                SubmissionTime = DateTime.Now,
+                Name = name,
+                TrainingDataPath = trainingDataPath,
+                ModelType = modelType
+            };
+
+            var hosts = _database.GetHosts();
+
+            if (hosts.Any())
+            {
+                var jobs = _database.GetJobs().Where(a => !a.Completed).ToList();
+
+                foreach (var host in hosts)
+                {
+                    if (jobs.Any(a => a.AssignedHost == host.Name))
+                    {
+                        continue;
+                    }
+
+                    job.AssignedHost = host.Name;
+
+                    break;
+                }
+
+                if (string.IsNullOrEmpty(job.AssignedHost))
+                {
+                    job.AssignedHost = lib.Common.Constants.UNASSIGNED_JOB;
+                }
+            }
+            else
+            {
+                job.AssignedHost = lib.Common.Constants.UNASSIGNED_JOB;
+            }
+
+            _database.AddJob(job);
+
+            return Index();
+        }
 
         [HttpGet]
         [Route("/Download")]
