2219712015b52862b771931619b8e0307b5dedfc
Jarred Capellman <jcapellman@hotmail.com>
2019-06-10 19:36:25 -0500

    Change: Migrated the PendingSubmissionsWorker to JobWorker to avoid duplicate work being performed

--- src/FileClassifier.JobManager.Worker/BackgroundWorkers/JobWorker.cs
+++ src/FileClassifier.JobManager.Worker/BackgroundWorkers/JobWorker.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 using FileClassifier.JobManager.lib.Databases;
@@ -12,19 +13,83 @@ using FileClassifier.lib.ML.Classification;
 using FileClassifier.lib.ML.Clustering;
 using FileClassifier.lib.Options;
 
+using Newtonsoft.Json;
+
+using NLog;
+
 namespace FileClassifier.JobManager.Worker.BackgroundWorkers
 {
     public class JobWorker
     {
+        private Logger Log = LogManager.GetCurrentClassLogger();
+
         private Hosts _host;
 
+        private readonly LiteDBDatabase _db;
+
         private string _serverURL;
+
+        private async void CheckPendingSubmissions()
+        {
+            var db = new LiteDBDatabase();
+
+            var pendingJobs = db.GetPendingSubmissions();
+
+            if (!pendingJobs.Any())
+            {
+                Log.Debug("No Pending Jobs found");
+
+                return;
+            }
+
+            Log.Debug($"{pendingJobs.Count} pending jobs found...");
+
+            var workerHandler = new WorkerHandler(_serverURL);
+
+            foreach (var pJob in pendingJobs)
+            {
+                Jobs job = null;
+
+                try
+                {
+                    job = JsonConvert.DeserializeObject<Jobs>(pJob.JobJSON);
+                }
+                catch (Exception ex)
+                {
+                    Log.Error($"For Job ID {pJob.ID}, could not parse {pJob.JobJSON} into a Jobs object due to {ex}");
+                }
+
+                if (job == null)
+                {
+                    Log.Error($"Job was null - removing {pJob.ID} from Queue");
+
+                    _db.RemoveOfflineSubmission(pJob.ID);
+
+                    continue;
+                }
+
+                var result = await workerHandler.UpdateWorkAsync(job);
+
+                if (result)
+                {
+                    Log.Debug($"{job.ID} was successfully uploaded");
+
+                    _db.RemoveOfflineSubmission(pJob.ID);
+
+                    continue;
+                }
+
+                Log.Debug($"{job.ID} was not able to be uploaded - will retry at a later date and time");
+            }
+        }
 
         public async Task<bool> Run(Hosts host, string serverURL)
         {
             _host = host;
 
             _serverURL = serverURL;
+
+            CheckPendingSubmissions();
 
             var workerHandler = new WorkerHandler(_serverURL);
 
@@ -106,7 +171,7 @@ namespace FileClassifier.JobManager.Worker.BackgroundWorkers
 
             db.AddOfflineSubmission(work);
 
-            // LOG HERE
+            Log.Debug($"{work.ID} has been added to the pending submission");
         }
     }
 }
